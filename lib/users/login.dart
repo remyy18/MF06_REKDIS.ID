a7dcb7eb16be25160b0872a98b93ff3487054ded
rizaiman <riza2001@mhs.unsyiah.ac.id>
2022-04-26 15:04:53 +0700

    improve login screen

--- lib/users/login.dart
+++ lib/users/login.dart
@@ -5,37 +5,47 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Selamat Datang!',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Image.asset('assets/images/login.png'),
-          const CupertinoTextField(),
-          const CupertinoTextField(),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Lupa Password'),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Login'),
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.blue,
-              primary: Colors.white,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 35.0,
-                vertical: 10.0,
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: [
+            const Text(
+              'Selamat Datang!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
               ),
-              shape: const StadiumBorder(),
             ),
-          ),
-          const Text('Belum punya akun? Sign Up'),
-        ],
+            Expanded(child: Image.asset('assets/images/login.png')),
+            const CupertinoTextField(),
+            const CupertinoTextField(),
+            Row(
+              children: [
+                Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Lupa Password'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Login'),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                primary: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 35.0,
+                  vertical: 10.0,
+                ),
+                shape: const StadiumBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Text('Belum punya akun? Sign Up'),
+          ],
+        ),
       ),
     );
   }
